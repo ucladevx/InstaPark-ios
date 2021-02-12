a1177c270c484cd18a71d543b46a6df6269ccf32
Yili Liu <akoliu8@gmail.com>
2021-02-11 22:41:52 -0500

    implement collection view for uploaded images on booking view

---
+++ InstaPark/Views/BookingImageCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  BookingImageCollectionViewCell.swift
+//  InstaPark
+//
+//  Created by Yili Liu on 2/10/21.
+//
+
+import Foundation
+import UIKit
+
+class BookingImageCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var image: UIImageView!
+}
